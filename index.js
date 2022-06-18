5e32b7d4f6391cdc0fee8d1ff5cc007cedb58402
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-06-18 17:11:59 +0600

    delete action add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -40,10 +41,18 @@ async function run() {
         });
 
         // Get Users 
-        app.get('/admin' , async (req, res) => {
+        app.get('/admin', async (req, res) => {
             const adminFind = adminData.find({});
             const result = await adminFind.toArray();
             res.send(result);
+        })
+
+        // Delete Users
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await adminData.deleteOne(query)
+            res.json(result)
         })
 
 
